78aef0926b603c1255d2d1f1d8f369c88d726de0
Mattia Giuffrida <giuffrida.mattia@gmail.com>
2022-08-19 14:17:26 +0100

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Main module for application helpers
+module ApplicationHelper
+end
---
+++ packs/demo_pack/spec/demo_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+describe 'RailsDemoApp' do
+  raise 'This should raise when running rspec from the root'
+end
